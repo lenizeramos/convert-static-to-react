2f7afecf4bb64ef8d3cd4a1d643d37ff908432ce
Lenize Ramos <lenizemr@gmail.com>
2025-01-16 23:20:51 -0800

    completed

--- frontend/src/components/Paint.tsx
+++ frontend/src/components/Paint.tsx
@@ -6,7 +6,7 @@ const Paint = () => {
       <section className="paint-section about-text left-container" id="paint">
         <h5 data-aos="zoom-out">{data.paint.titleEN}</h5>
         <h3 data-aos="zoom-out">{data.paint.titleCN}</h3>
-
+        <div className="horizontal-divider"></div>
         <img
           src="./images/block_paint_img.png"
           alt="alt-block_paint_img.png"
@@ -20,6 +20,7 @@ const Paint = () => {
               </p>
             ))}
           </div>
+          
           <div className="right parallax-scroll">
             {data.paint.content.right.map((paragraph, index) => (
               <p data-aos="zoom-out" key={index}>
